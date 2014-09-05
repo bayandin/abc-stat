9f0a704de9d6d87d75044bd49efe5f562da41eed
Alexander Bayandin <a.bayandin@gmail.com>
2014-09-05 19:10:30 +0400

    Initial commit

---
+++ process.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+#
+# http://www.prooffreader.com/2014/05/graphing-distribution-of-english.html
+
+import matplotlib.pyplot as plt
+
+ALPHABET = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+BUCKETS_COUNT = 720
+FILENAME = 'data/ru.txt'
+
+if __name__ == '__main__':
+    stat = {c: [0] * BUCKETS_COUNT for c in ALPHABET}
+
+    with open(FILENAME, 'r') as f:
+        for line in f:
+            lowered_stripped_line = line.strip().lower()
+
+            for i, c in enumerate(lowered_stripped_line):
+                if c not in ALPHABET:
+                    continue
+
+                part = BUCKETS_COUNT // len(lowered_stripped_line)
+                for j in range(i * part, (i + 1) * part):
+                    stat[c][j] += 1
+
+    for c in ALPHABET:
+        fig = plt.figure(figsize=(10, 10))
+        plt.fill_between(
+            range(BUCKETS_COUNT),
+            stat[c]
+        )
+        fig.savefig('result/%s.png' % c)
